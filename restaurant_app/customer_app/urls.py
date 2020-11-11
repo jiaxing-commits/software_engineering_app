955fe1395eb20d48f29d5b47a68a298e1120304f
James <Jiawu1998@github.com>
2020-11-11 15:32:21 -0600

    5 pages done

---
+++ restaurant_app/customer_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.default_home, name='default_home'),
+    path('logged', views.logged_home, name='logged_home'),
+    path('menu', views.menu, name='menu'),
+    path('checkout', views.checkout, name='checkout'),
+]
