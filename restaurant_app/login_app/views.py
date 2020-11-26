08c2eeae6b1ed23ee36f7f593cef836995f01c24
James <Jiawu1998@github.com>
2020-11-26 17:07:37 -0600

    completed log in mechanic

--- restaurant_app/login_app/views.py
+++ restaurant_app/login_app/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from db_models.models import Customer, Staff
 
 def login(request):
+
+    if request.method == 'GET':
+        if 'username' in request.GET and 'password' in request.GET:
+            if Customer.objects.filter(email=request.GET['username'], password=request.GET['password']).exists():
+                return redirect('/../logged')
+        return render(request, 'login_app/login.html')
+
+
     return render(request, 'login_app/login.html')
