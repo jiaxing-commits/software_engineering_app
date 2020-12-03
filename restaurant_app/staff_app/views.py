9be993df77956898e3b1d55b28d7fa8d7c5a3ba7
YIK YU LAU <ylau1@hawk.iit.edu>
2020-12-02 20:30:24 -0600

    added prep time

--- restaurant_app/staff_app/views.py
+++ restaurant_app/staff_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from db_models.models import Current_Orders, Customer, Order_history, Staff
+import json
 
 # Create your views here.
 
@@ -25,15 +26,24 @@ def employee_portal(request):
                 
         total_orders = Current_Orders.objects.all()
         request_status = []
+        all_prep_time = []
         for i in total_orders: 
+            y = json.loads(i.item_list)
+            add_price = len(y) * 2
+            prep_time = 30 + add_price
+            all_prep_time.append(prep_time)
             if i.customer_id != -1:
                 customer = Customer.objects.get(customer_id = i.customer_id) 
                 request_status.append(customer.customer_request_status)
             else:
                 request_status.append("In Progress")
-        total_orders = [[x,y] for (x,y) in zip(total_orders, request_status)]
-        context = { 'total_orders': total_orders }
+        total_orders = [[x,y,z] for (x,y,z) in zip(total_orders, request_status, all_prep_time)]
+        context = { 'total_orders': total_orders}
 
+    # total_items = 0
+
+    # for x, y in cart.items():
+    #     total_items += y[1]
 
         return render(request, 'staff_app/employee_portal.html', context)
     else:
