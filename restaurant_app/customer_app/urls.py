88aecde8c67e6028a5d99aed03b80e32bc3b8157
James <Jiawu1998@github.com>
2022-05-14 23:54:10 -0500

    fixed to url paths

--- restaurant_app/customer_app/urls.py
+++ restaurant_app/customer_app/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.default_home, name='default_home'),
-    path('logged', views.logged_home, name='logged_home'),
-    path('menu', views.menu, name='menu'),
-    path('checkout', views.checkout, name='checkout'),
-    path('customer_account_creation_form', views.customer_account_creation_form, name='customer_account_creation_form'),
-    path('order_history', views.order_history, name='order_history'),
+    path('logged/', views.logged_home, name='logged_home'),
+    path('menu/', views.menu, name='menu'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('customer_account_creation_form/', views.customer_account_creation_form, name='customer_account_creation_form'),
+    path('order_history/', views.order_history, name='order_history'),
 ]
--- restaurant_app/staff_app/urls.py
+++ restaurant_app/staff_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('employee_portal', views.employee_portal, name='employee_portal'),
+    path('employee_portal/', views.employee_portal, name='employee_portal'),
 ]
